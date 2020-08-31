0f700178614d359b45e61132f146d853612b0115
Ignacio Jonas <ignaciojonas@gmail.com>
2020-08-31 10:35:19 -0300

    Closes #2 - Aplicar el feedback del PR

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,7 +4,7 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Hola Mundo!</title>
+        <title>Hola Mundo</title>
 
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Hola Mundo!
+                    Hola Mundo
                 </div>
             </div>
         </div>
