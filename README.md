1d688ad48216922e15b29d188a7f031f9737e12f
Ignacio Jonas <ignaciojonas@gmail.com>
2020-09-17 09:37:47 -0300

    Configure Dusk in GHA

--- README.md
+++ README.md
@@ -1,4 +1,5 @@
 # My Project
 [![codecov](https://codecov.io/gh/ignaciojonas/my-project/branch/master/graph/badge.svg)](https://codecov.io/gh/ignaciojonas/my-project)
 ![Static Code Analysis](https://github.com/ignaciojonas/my-project/workflows/Static%20Code%20Analysis/badge.svg)
-![Laravel](https://github.com/ignaciojonas/my-project/workflows/Laravel/badge.svg)
+![Unit Tests](https://github.com/ignaciojonas/my-project/workflows/Unit%20Tests/badge.svg)
+![Dusk Tests](https://github.com/ignaciojonas/my-project/workflows/Dusk%20Tests/badge.svg)
--- tests/DuskTestCase.php
+++ tests/DuskTestCase.php
@@ -32,11 +32,11 @@ abstract class DuskTestCase extends BaseTestCase
         $options = (new ChromeOptions)->addArguments([
             '--disable-gpu',
             '--headless',
-            '--window-size=1920,1080',
+            '--window-size=1920,1080'
         ]);
-
+        
         return RemoteWebDriver::create(
-            'http://selenium:4444/wd/hub', DesiredCapabilities::chrome()->setCapability(
+            env('SELENIUM_URL', 'http://localhost:9515'), DesiredCapabilities::chrome()->setCapability(
                 ChromeOptions::CAPABILITY, $options
             )
         );
